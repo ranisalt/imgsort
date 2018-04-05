14b642e97daf9434be16fa3c9922a42174f05835
Ranieri Althoff <ranisalt@gmail.com>
2018-04-04 23:07:20 -0300

    Remove dedup stuff, generate whitelist on runtime

--- imgsort.py
+++ imgsort.py
@@ -2,92 +2,47 @@
 import logging
 import os
 import shutil
+from collections import Counter
 from functools import lru_cache
-from typing import Any, Callable, Dict, List, Set, Tuple
+from typing import Any, Callable, Dict, FrozenSet, List, Tuple
 
 from dataclasses import dataclass, field
 from PIL import Image
 
 __version__ = '0.1'
 
-try:
-    from image_match.goldberg import ImageSignature
-
-    # Signature generator
-    gis = ImageSignature()
-
-    def distance(a: float, b: float) -> float:
-        return gis.normalized_distance(a, b)
-
-    def signature(image: 'ImageMetadata') -> float:
-        return gis.generate_signature(image.filename)
-
-    IMAGE_MATCH_ENABLED = True
-except ImportError:
-    IMAGE_MATCH_ENABLED = False
-
 # Setup logging facility
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fmt = logging.Formatter('[%(levelname)s - %(filename)s:%(lineno)d] - %(message)s')
 ch.setFormatter(fmt)
 logger.addHandler(ch)
 
 
+# Typing aliases
+Resolution = Tuple[int, int]
+
+
 @dataclass
 class ImageMetadata:
-    image: Image
-    _signature: float = field(init=False)
+    filename: os.PathLike
+    resolution: Resolution
 
     @property
     def basename(self):
-        return os.path.basename(self.image.filename)
+        return os.path.basename(self.filename)
 
     @property
     def dirname(self):
-        return os.path.dirname(self.image.filename)
-
-    @property
-    def filename(self):
-        return self.image.filename
-
-    @property
-    def resolution(self):
-        return self.image.size
-
-    @property
-    def signature(self):
-        if not self._signature:
-            self._signature = signature(self)
-        return self._signature
+        return os.path.dirname(self.filename)
 
 
-# Typing aliases
-ImageMap = Dict[os.PathLike, ImageMetadata]
-Resolution = Tuple[int, int]
-
-WHITELIST: Set[Resolution] = {
-    (1366, 768),
-    (1440, 900),
-    (1600, 900),
-    (1680, 1050),
-    (1920, 1080),
-    (1920, 1200),
-    (1920, 1280),
-    (1920, 1440),
-    (2560, 1440),
-    (2560, 1600),
-    (2880, 1800),
-    (3840, 1080),
-    (3840, 1200),
-    (3840, 2160),
-}
-
-
-def prepare(paths: List[os.PathLike], similarity: bool) -> List[ImageMetadata]:
+def prepare(paths: List[os.PathLike], similarity: bool) \
+        -> Tuple[List[ImageMetadata], FrozenSet[Resolution]]:
     images: List[ImageMetadata] = []
+    whitelist = Counter()
 
     for path in paths:
         try:
@@ -97,43 +52,27 @@ def prepare(paths: List[os.PathLike], similarity: bool) -> List[ImageMetadata]:
             continue
 
         logger.debug(f'Processing metadata for {path}')
-        images.append(ImageMetadata(image=image))
+        images.append(ImageMetadata(filename=path, resolution=image.size))
+        whitelist[image.size] += 1
 
     logger.info(f'Found {len(images)} images')
-    return images
+    return images, frozenset({res for res in whitelist if whitelist[res] > 4})
 
 
-def trim_by_whitelist(images: List[ImageMetadata]) -> List[ImageMetadata]:
-    return [image for image in images if image.resolution in WHITELIST]
-
-
-# TODO
-def trim_by_similarity(images: List[ImageMetadata]) -> List[ImageMetadata]:
-    return []
-
-
-@lru_cache()
-def path_to_resolution(dirname: os.PathLike, resolution: Resolution) \
-        -> os.PathLike:
-    if resolution in WHITELIST:
-        return os.path.join(dirname, '{}x{}'.format(*resolution))
-    return dirname
-
-
-def path_to_image(dirname: os.PathLike, image: ImageMetadata) -> os.PathLike:
-    path = path_to_resolution(dirname, image.resolution)
-    return os.path.join(path, image.basename)
-
-
-def scatter(images: List[ImageMetadata], output: os.PathLike) \
+def scatter(images: List[ImageMetadata],
+            whitelist: FrozenSet[Resolution],
+            dirname: os.PathLike) \
         -> Tuple[List[os.PathLike], Dict[os.PathLike, os.PathLike]]:
-    needed_dirs = set()
-    if not os.path.isdir(output):
-        needed_dirs.add(output)
+    @lru_cache()
+    def path_to_resolution(resolution: Resolution) -> os.PathLike:
+        if resolution in whitelist:
+            return os.path.join(dirname, '{}x{}'.format(*resolution))
+        return dirname
 
     mapping = dict()
+    needed_dirs = {dirname}
     for image in images:
-        destdir = path_to_resolution(output, image.resolution)
+        destdir = path_to_resolution(image.resolution)
         dest = os.path.join(destdir, image.basename)
 
         if os.path.isfile(dest) and os.path.samefile(image.filename, dest):
@@ -145,12 +84,12 @@ def scatter(images: List[ImageMetadata], output: os.PathLike) \
     return needed_dirs, mapping
 
 
-def create_dirs(dirs: List[os.PathLike], dry_run: bool):
-    for dir_ in dirs:
-        if not os.path.isdir(dir_):
-            logger.info(f'mkdir {dir_}')
+def create_dirs(dirnames: List[os.PathLike], dry_run: bool):
+    for dirname in dirnames:
+        if not os.path.isdir(dirname):
+            logger.info(f'mkdir {dirname}')
             if not dry_run:
-                os.makedirs(dir_)
+                os.makedirs(dirname)
 
 
 def copy_or_move(mapping: Dict[os.PathLike, os.PathLike],
@@ -167,18 +106,9 @@ def main():
     parser = argparse.ArgumentParser(
         description='Automatic wallpaper sorter by image dimensions')
 
-    parser.add_argument('-i', '--ignore', action='store_true',
-                        help='ignore files not in resolution whitelist')
     parser.add_argument('-m', '--move', action='store_const',
                         const=shutil.move, default=shutil.copyfile)
     parser.add_argument('-n', '--dry-run', action='store_true')
-
-    if IMAGE_MATCH_ENABLED:
-        match = parser.add_argument_group('similarity detection')
-        match.add_argument('-s', '--similar', action='store_true',
-                           help='try and find similar images')
-        match.add_argument('-t', '--threshold', type=float, default=0.4,
-                           metavar='T', help='similarity threshold')
 
     # TODO: watch folder
 
@@ -187,14 +117,9 @@ def main():
 
     args = parser.parse_args()
 
-    images = prepare(args.images, args.similar)
+    images, whitelist = prepare(args.images, args.similar)
 
-    if args.ignore:
-        count = len(images)
-        images = trim_by_whitelist(images)
-        logger.debug(f'Trimmed {count - len(images)} images by whitelist')
-
-    needed_dirs, mapping = scatter(images, args.output)
+    needed_dirs, mapping = scatter(images, whitelist, args.output)
     create_dirs(needed_dirs, args.dry_run)
     copy_or_move(mapping, args.move, args.dry_run)
 
